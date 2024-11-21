b2482644fefcec9daf0320509adbac2fb8160992
Jonas-Bouterfas <Jonas.bouterfas@gmail.com>
2024-11-21 09:59:20 +0100

    Förtydligande

--- app/src/main/java/com/example/threeconnect/MainActivity.kt
+++ app/src/main/java/com/example/threeconnect/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             ThreeConnectTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Jonas")
+                    Greeting("Jonas123")
                 }
             }
         }
