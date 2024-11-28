c94bfddcf817436c96be0c95300bce60a0fbcaba
Jonas-Bouterfas <Jonas.bouterfas@gmail.com>
2024-11-28 15:41:02 +0100

    added a player class and a game class

--- app/src/main/java/com/example/threeconnect/GameModel.kt
+++ app/src/main/java/com/example/threeconnect/GameModel.kt
@@ -3,3 +3,11 @@ package com.example.threeconnect
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 
+data class Player( var name: String = "")
+
+data class Game(
+    var gameBoard: List<Int> = List(9) { 0 },
+    var gameState: String = "invite",
+    var player1Id: String = "",
+    var player2Id:
+)
